4fc85d403ac6e437665da163dc4e30dc47efe2ec
jsurya9311 <jsurya9311@gmail.com>
2019-08-19 08:21:11 +0530

    :#

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+//import './App.css';
+import HomePage from './homepage.component';
 
 function App() {
   return (
     <div className="App">
-      <h1>Sample React App</h1>
+      <HomePage />
     </div>
   );
 }
